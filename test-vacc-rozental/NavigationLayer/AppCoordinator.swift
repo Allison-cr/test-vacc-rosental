ed1a1d758fe1d13fc9f2cab3b0c1071978c6b2ba
Allison <suprun-s@bk.ru>
2024-08-22 04:57:16 +0300

    add navigation layer

--- test-vacc-rozental/NavigationLayer/AppCoordinator.swift
+++ test-vacc-rozental/NavigationLayer/AppCoordinator.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+import UIKit
+
+// MARK: - AppCoordinator
+
+final class AppCoordinator: BaseCoordinator {
+    
+    // MARK: - Properties
+    
+    /// The main navigation controller used to manage view controllers.
+      private let navigationController: UINavigationController
+
+    // MARK: - Initializer
+    
+    /// Initializes the `AppCoordinator` with a specified navigation controller.
+    /// - Parameter navigationController: The navigation controller that will manage the app's navigation stack.
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+     
+    // MARK: - Coordinator Lifecycle
+    /// Starts the coordinator by initiating the main navigation flow of the application.
+    override func start() {
+        runMainFlow()
+    }
+    
+    // MARK: - Private Methods
+    
+    /// Configures and starts the main flow of the application.
+    /// This typically involves creating the primary view controllers and presenting them
+    private func runMainFlow() {
+        let coordinator = MainCoordinator(navigationController: navigationController)
+        addChild(coordinator)
+        coordinator.start()
+    }
+}
